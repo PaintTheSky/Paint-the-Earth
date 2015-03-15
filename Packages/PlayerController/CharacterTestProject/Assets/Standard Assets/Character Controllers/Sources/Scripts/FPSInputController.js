7b73447c7490d3b52b12458b10b7d670e113934a
unknown <fzs7217@GDD-7.main.ad.rit.edu>
2015-03-15 17:40:52 -0400

    Finished turning

--- Packages/PlayerController/CharacterTestProject/Assets/Standard Assets/Character Controllers/Sources/Scripts/FPSInputController.js
+++ Packages/PlayerController/CharacterTestProject/Assets/Standard Assets/Character Controllers/Sources/Scripts/FPSInputController.js
@@ -1,5 +1,8 @@
 private var motor : CharacterMotor;
-
+public var Direction : int; // 0 north, 1 east, 2 south, 3 west
+public var TurnAllowed : boolean; // one turn per button press
+public var DirectionString : String; // tells you direction in unity editor
+public var ForwardVector : Vector3;
 // Use this for initialization
 function Awake () {
 	motor = GetComponent(CharacterMotor);
@@ -8,7 +11,26 @@ function Awake () {
 // Update is called once per frame
 function Update () {
 	// Get the input vector from keyboard or analog stick
-	var directionVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	var directionByInput = Input.GetAxis("Horizontal");
+	
+	// left
+	if(directionByInput > 0 && TurnAllowed){
+		turnRight();
+		TurnAllowed = false;
+	}
+	// right
+	else if(directionByInput < 0 && TurnAllowed){
+		turnLeft();
+		TurnAllowed = false;
+	}
+	else if(directionByInput == 0) { TurnAllowed = true; } 
+	
+	var directionVector = 
+		new Vector3(
+			(Direction==1)? 0.5 : ((Direction==3)? -0.5: 0), // for West and East (+/- X direction)
+			0, 
+			(Direction==0)? 0.5 : ((Direction==2)? -0.5: 0)); // for North and South (+/- Z direction)
+	ForwardVector = directionVector;
 	
 	if (directionVector != Vector3.zero) {
 		// Get the length of the directon vector and then normalize it
@@ -27,9 +49,35 @@ function Update () {
 		directionVector = directionVector * directionLength;
 	}
 	
+	DirectionString = getDirectionString(Direction);
 	// Apply the direction to the CharacterMotor
 	motor.inputMoveDirection = transform.rotation * directionVector;
 	motor.inputJump = Input.GetButton("Jump");
+}
+
+public function getDirectionString(dir: int){
+	switch(dir){
+		case 0:
+			return "North";
+		case 1:
+			return "East";
+		case 2: 
+			return "South";
+		case 3:
+			return "West";
+		default: 
+			return "WAT?";
+	}
+}
+
+function turnRight(){
+	Direction = (Direction + 1);
+	if(Direction > 3) { Direction -= 4; }
+}
+
+function turnLeft(){
+	Direction = (Direction - 1);
+	if(Direction < 0) { Direction += 4; }
 }
 
 // Require a character controller to be attached to the same game object
